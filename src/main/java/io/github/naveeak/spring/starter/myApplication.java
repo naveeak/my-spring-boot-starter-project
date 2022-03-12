1df5c081c837a6359039119be6c12d00a61d7563
ak <ak7195664@gmail.com>
2022-03-12 19:06:16 +0530

    added package

--- src/main/java/io/github/naveeak/spring/starter/myApplication.java
+++ src/main/java/io/github/naveeak/spring/starter/myApplication.java
@@ -1,3 +1,5 @@
+package io.github.naveeak.spring.starter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
