7f1a6acaabe30cda496ed7923bd81877f69a6b4a
ak <ak7195664@gmail.com>
2022-03-12 14:56:26 +0530

    initial commit

---
+++ src/main/java/myApplication.java
@@ -0,0 +1,18 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class myApplication {
+
+    @RequestMapping("/")
+    String home(){
+        return "Hello world!";
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(myApplication.class ,args);
+    }
+}
